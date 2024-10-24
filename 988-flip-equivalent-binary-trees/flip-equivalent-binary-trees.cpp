6dc6f85ab2fa35ecbda7b977bc88c1d5ddf1060c
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-10-25 01:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (21.10%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -6,123 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool solve(TreeNode* r1, TreeNode* r2) {
-        if(!r1 && !r2){
+    bool solve(TreeNode* r1,TreeNode* r2){
+        if(!r1&&!r2){
             return true;
         }
-        if(!r1||!r2){
+        if(!r1 || !r2 || r1->val != r2->val){
             return false;
         }
 
-        cout<<"r1:"<<r1->val<<endl;
-        cout<<"r2:"<<r2->val<<endl;
-
-        
-      
-        int count=0;
-        if(!r1->left) count+=1;
-         if(!r1->right) count+=1;
-          if(!r2->left) count+=1;
-           if(!r2->right) count+=1;
-
-        if(count==1||count==3){
-            return false;
-        }
-
-        bool a=false;
-        bool b=false;
-
-        //all four exist 
-        if(r1->left && r2->left && r1->right && r2->right){
-            if(r1->left->val==r2->left->val){
-                if(r1->right->val==r2->right->val){
-                    a=solve(r1->left,r2->left);
-                     b=solve(r1->right,r2->right);
-
-                }
-            }
-            else{
-                //swap
-                TreeNode* temp=r1->left;
-                r1->left=r1->right;
-                r1->right=temp;
-                  if(r1->left->val==r2->left->val){
-                if(r1->right->val==r2->right->val){
-                    a=solve(r1->left,r2->left);
-                     b=solve(r1->right,r2->right);
-
-                }
-             }
-            }
-        }
-
-        //only two exist 
-        else if(r1->left && r2->left){
-            if(r1->left->val==r2->left->val){
-                if(!r1->right && !r2->right){
-                     a=solve(r1->left,r2->left);
-                     b=true;
-                     }
-            }
-        }
-
-         else if(r1->right && r2->right){
-            if(r1->right->val==r2->right->val){
-                if(!r1->left && !r2->left){
-                     a=solve(r1->right,r2->right);
-                     b=true;
-                     }
-            }
-        }
-
-        //only two ->need to swap
-        else if(r1->left && r2->right ){
-            if(r1->left->val==r2->right->val){
-                if(!r1->right && !r2->left){
-                    TreeNode* temp=r1->left;
-                    r1->right=temp;
-                    r1->left=NULL;
-                    a=true;
-                    b=solve(r1->right,r2->right);
-                }
-            }
-        }
-
-         else if(r1->right && r2->left ){
-            if(r1->right->val==r2->left->val){
-                if(!r1->left && !r2->right){
-                    TreeNode* temp=r1->right;
-                    r1->left=temp;
-                    r1->right=NULL;
-                    b=true;
-                    a=solve(r1->left,r2->left);
-                    b=true;
-                }
-            }
-        }
-
-        else if(!r1->left||!r2->left||!r1->right||!r2->right) return true;
-         cout<<"a:"<<a<<endl;
-        cout<<"b:"<<b<<endl;
-        return a&&b;
+        return ((solve(r1->left,r2->left) || solve(r1->left,r2->right)) &&( solve(r1->right,r2->right) || solve(r1->right,r2->left)));
 
     }
+
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if(root1 && root2){
-            if(root1->val!=root2->val){
-                return false;
-            }
-        }
-        else{
-            if(!root1 && !root2) return true;
-            if(!root1 || !root2) return false;
-        }
-        return solve(root1, root2);
+        return solve(root1,root2);
     }
 };
