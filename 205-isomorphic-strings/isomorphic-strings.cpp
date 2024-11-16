b9bd1c6fccf8abc0a692bb0707095d2598fdf388
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-11-16 19:41:48 +0530

    Time: 3 ms (52.52%) | Memory: 8.5 MB (86.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> check;
-        unordered_map<char, char> m;
-        if (s.size() != t.size())
-            return false;
-        for (int i = 0; i < s.size(); i++) {
-            if (check.count(s[i])) {
-                // get the char
-                char c = check[s[i]];
-                // check if equal to the current char in string t
-                // if not early return
-                if (c != t[i])
-                    return false;
+        vector<int> v(200,-1);
+        vector<int> w(200,-1);
 
-                // else replace
-                s[i] = c;
-            } else {
-                if (!m.count(t[i]) || m[t[i]] == s[i]) {
-                    // create a mapping of the current char of s to t
-                    check[s[i]] = t[i];
-                    //create a reverse mapping to check for the same map condn
-                    m[t[i]]=s[i];
-                    // replace t
-                    s[i] = t[i];
-                } else {
-                    return false;
-                }
-            }
+        if(s.size() != t.size()) return false;
+        
+        for(int i =0;i<s.size();i++){
+            v[s[i]] = t[i];
+            w[t[i]] = s[i];
         }
-        cout << "S:" << s << endl;
-        cout << "T:" << t << endl;
-        return s == t;
+
+        for(int i =0;i<s.size();i++)
+        {
+            char temp = s[i];
+            s[i] = (char)v[s[i]];
+            // cout<<temp<<" "<<s[i]<<endl;
+            if(w[s[i]] != temp) return false;
+        }
+
+        if(s == t) return true;
+        return false;
     }
 };
