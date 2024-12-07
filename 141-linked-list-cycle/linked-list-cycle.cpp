3179b67c35a020ad790a739b0251730f4e449b8a
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-12-07 16:18:29 +0530

    Time: 6 ms (90.35%) | Memory: 11.8 MB (25.22%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -10,15 +10,22 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if(head==NULL) return false;
-        //making all INT_MIN,check if next of any is INT_MIN;
-        ListNode* temp=head;
-        while(temp->next!=NULL){
-            if(temp->next->val==INT_MIN) return true;
-            temp->val=INT_MIN;
-            temp=temp->next;
+        if(head->next==NULL) return false;
+        if(head->next==head) return true;
+        //SLOW AND FAST POINTER
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+
+        while(slow!=fast && slow->next!=NULL){
+            slow=slow->next;
+            if(fast->next) fast=fast->next;
+            if(fast->next) fast=fast->next;
+
         }
 
 
-        return false;
-    }
+        if(slow->next==NULL) return false;
+        return true;
+}
+     
 };
