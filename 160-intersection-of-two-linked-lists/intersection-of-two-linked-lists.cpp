7972749b1c0804249933853ba90f0dbd537c7712
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-12-13 23:25:50 +0530

    Time: 42 ms (28.14%) | Memory: 18.6 MB (18.16%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,78 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* tempA=headA;
-        ListNode* tempB=headB;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        if (!headA || !headB) return nullptr;
 
-        int lengthA=1;
-        int lengthB=1;
+        ListNode* pA = headA;
+        ListNode* pB = headB;
 
-        while(tempA->next!=NULL){
-            lengthA++;
-            tempA=tempA->next;
+        while (pA != pB) {
+            pA = (pA == nullptr) ? headB : pA->next;
+            pB = (pB == nullptr) ? headA : pB->next;
         }
 
-        while(tempB->next!=NULL){
-            lengthB++;
-            tempB=tempB->next;
-        }
-
-     
-
-        //figure out the bigger list
-        if(lengthB>=lengthA) {
-            int diff=lengthB-lengthA;
-            tempB=headB;
-            tempA=headA;
-            int cnt=0;
-            //move b to a level
-            while(cnt!=diff){
-                tempB=tempB->next;
-                cnt++;
-            }
-          
-            // //now move until they become equal
-            while(tempA!=tempB){
-              
-                tempB=tempB->next;
-                tempA=tempA->next;
-               
-            }
-
-            return tempB;
-
-        }
-        else{
-            int diff=lengthA-lengthB;
-            
-            tempB=headB;
-            tempA=headA;
-            int cnt=0;
-            //move a to a level
-            while(cnt!=diff){
-                tempA=tempA->next;
-                cnt++;
-            }
-            
-
-            // //now move until they become equal
-            while(tempA!=tempB){
-               
-                tempB=tempB->next;
-                tempA=tempA->next;
-            }
-
-            return tempA;
-        }
-
-        return tempA;
+        return pA; // Either intersection node or nullptr
     }
 };
