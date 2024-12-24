b3cba1745c87ec838c0e653b483e4f74395621f3
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-12-25 01:53:45 +0530

    Time: 3 ms (16.15%) | Memory: 10.3 MB (5.03%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,54 +1,32 @@
 class Solution {
 public:
+    int solve(string s,int i,int sign,long long res){
+        if(i>=s.size() || !isdigit(s[i])) return res*sign;
+
+        res=(res*10)+s[i]-'0';
+        
+        if(res*sign<=INT_MIN) return INT_MIN;
+        if(res*sign>=INT_MAX) return INT_MAX;
+
+        return solve(s,i+1,sign,res);
+    }
     int myAtoi(string s) {
-        int n = s.size();
-        long long num = 0;
-        int i = 0;
-        bool neg = false;
-        for (; s[i] == ' '; i++)
-            ;
-        int count = 0;
-        for (; s[i] == '+' || s[i] == '-'; i++) {
-            count += 1;
-            if (s[i] == '-')
-                neg = true;
+        int i=0;            
+        for(;s[i]==' ';i++);
+        if(i==s.size()) return 0;
+        string j=s.substr(i);
+        if(j.empty()) return 0;
+        int index=0; 
+        int sign=1;
+        if(s[i]=='+'){
+            index=1;
+            sign=1;
         }
-        if (count > 1)
-            return 0;
-        long long mul = 10;
-        for (; s[i] == '0'; i++)
-            ;
-        bool greaterInt=false;
-        for (; i < n; i++) {
-
-            if (s[i] == '0' || s[i] == '1' || s[i] == '2' || s[i] == '3' ||
-                s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' ||
-                s[i] == '8' || s[i] == '9') {
-                num = (num * mul);
-                num += (s[i] - '0');
-
-            } else {
-                break;
-            }
-
-            if (num > INT_MAX) {
-                num = INT_MAX;
-                greaterInt=true;
-                break;
-            }
+        else if(s[i]=='-'){
+            index=1;
+            sign=-1;
         }
-        if (neg) {
-                if(num==INT_MAX && greaterInt){
-                    num=INT_MIN;
-                    
-                }
-                else{
-                num -= (2 * num);
-                if (num < INT_MIN) {
-                    num = INT_MIN;
-                }
-                }
-        }
-        return num;
+
+        return solve(j,index,sign,0);
     }
 };
