9768cc34226721a063edab2e57e10f31c528dd42
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-12-30 23:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (53.81%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    vector<vector<int> > subsetsWithDup(vector<int>& nums) {
+  void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
+         ans.push_back(ds);
+         for (int i = ind; i < nums.size(); i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            ds.push_back(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.pop_back();
+         }
+      }
 
-
-        sort(nums.begin(), nums.end());
-        vector<int> temp ;
-        vector <vector <int>> ans ;
-        helper(0 , nums , temp , ans);
-
-        return ans;
-    
-    }
-
-  void helper( int ind , vector <int> nums , vector <int> &temp  , vector <vector<int>> &ans){
-
-    if(ind == nums.size()) {
-        ans.push_back(temp);
-        return ;
-    }
-
-    
-     temp.push_back(nums[ind]);
-
-      helper(ind +1 , nums , temp , ans);
-      temp.pop_back();
-   
-    while( ind +1 < nums.size() && nums[ind] == nums[ind +1]){
-        ind++;
-    }
-
-        helper(ind +1 , nums , temp , ans);
-    }
-
+ vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+         vector < vector < int >> ans;
+         vector < int > ds;
+         sort(nums.begin(), nums.end());
+         findSubsets(0, nums, ds, ans);
+         return ans;
+      }
   
 
 };
