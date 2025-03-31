7dd5f474269ce3001fa3f7f0d595ae31df9bee43
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-03-31 23:51:50 +0530

    Time: 455 ms (10.69%) | Memory: 52.2 MB (87.07%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()==1) return true;
+        int i=0;
+        while(i<nums.size()){
+            int curr=nums[i];
+            cout<<"I:"<<i<<endl;
+            if(curr==0) return false;
+            if(i+curr>=nums.size()-1) return true;
+            //find the max within that range
+            int maxi=INT_MIN;
+            int mi=i+1;
+            for(int j=i+1;j<=i+curr;j++){
+                if(j+nums[j]>maxi){
+                    maxi=j+nums[j];
+                    mi=j;
+                }
+            }
+
+            i=mi;
+
+
+        }
+        return false;
+    }
+};
