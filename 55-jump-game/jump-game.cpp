eae8e90cd45fe45f7d830eef948c6c78f3813d96
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-04-08 22:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (64.00%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size()==1) return true;
+        int maxind=0;
         int i=0;
-        while(i<nums.size()){
-            int curr=nums[i];
-            if(curr==0) return false;
-            if(i+curr>=nums.size()-1) return true;
-            //find the max within that range
-            int maxi=INT_MIN;
-            int mi=i+1;
-            for(int j=i+1;j<=i+curr;j++){
-                if(j+nums[j]>maxi){
-                    maxi=j+nums[j];
-                    mi=j;
-                }
-            }
-
-            i=mi;
-
-
+        int n=nums.size();
+        while(i<n){
+            if(i>maxind) return false;
+            maxind=max(maxind,nums[i]+i);
+            i++;
         }
-        return false;
+        return true;
     }
 };
