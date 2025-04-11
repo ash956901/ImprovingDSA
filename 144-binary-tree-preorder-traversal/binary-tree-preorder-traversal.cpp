5827b3bc7733bb112aa245ecec8d674ef8f7a584
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-04-12 01:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.76%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -16,31 +16,21 @@ public:
        
         vector<int> ans;
          if(root==NULL) return ans;
-        stack<pair<TreeNode*,int>> s;
-        s.push(make_pair(root,1));
+        stack<TreeNode*> s;
+        s.push(root);
         while(!s.empty()){
             auto top=s.top();
             s.pop();
 
-            if(top.second==1){
-                 ans.push_back(top.first->val);
-            }
-           
+            
+            ans.push_back(top->val);
 
-            if(top.second==1){
-                s.push(make_pair(top.first,2));
-                if(top.first->left){
-                    s.push(make_pair(top.first->left,1));
-                }
-            }
-
-            else if(top.second==2){
-                if(top.first->right){
-                    s.push(make_pair(top.first->right,1));
-                }
-            }
-
-           
+            if(top->right!=nullptr){
+                s.push(top->right);
+            } 
+             if(top->left!=nullptr){
+                s.push(top->left);
+            } 
         }
         return ans;
     }
