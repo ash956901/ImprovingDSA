899e1565dd6ef24bd131f142b41e240e544d89d8
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-05-04 20:50:47 +0530

    Time: 11 ms (5.08%) | Memory: 15.6 MB (30.42%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+   unordered_map<int,vector<int>> graph;
+
+   void buildGraph(TreeNode* root,TreeNode* parent){
+    if(root==nullptr) return;
+    if(parent){
+        graph[parent->val].push_back(root->val);
+        graph[root->val].push_back(parent->val);
+
+    }
+
+    buildGraph(root->left,root);
+    buildGraph(root->right,root);
+    return;
+   }
+
+
+   vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int>  ans;
+        buildGraph(root,nullptr);
+        queue<pair<int,int>> q;
+        q.push(make_pair(target->val,0));
+        unordered_map<int,bool> visited;
+        visited[target->val]=true;
+        while(!q.empty()){
+            auto curr=q.front();
+            q.pop();
+
+            int nodeVal=curr.first;
+            int nodedist=curr.second;
+
+            if(nodedist==k){
+                ans.push_back(nodeVal);
+            }
+            else if(nodedist<k){
+                for(auto i:graph[nodeVal]){
+                    if(!visited[i]){
+                        visited[i]=true;
+                        q.push(make_pair(i,nodedist+1));
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
