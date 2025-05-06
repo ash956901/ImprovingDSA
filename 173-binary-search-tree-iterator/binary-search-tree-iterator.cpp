831ddc91b407989be3af32e87445841b25239f0f
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-05-06 21:04:53 +0530

    Time: 43 ms (5.73%) | Memory: 32 MB (69.61%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    BSTIterator(TreeNode* root) {
+        TreeNode* temp=root;
+        while(temp!=nullptr){
+            s.push(temp);
+            temp=temp->left;
+        }
+    }
+    
+    int next() {
+        TreeNode* temp=s.top();
+        int ret=temp->val;
+        s.pop();
+        if(temp->right!=nullptr){
+            temp=temp->right;
+            while(temp!=nullptr) {
+                s.push(temp);
+                temp=temp->left;
+            }
+        }
+        cout<<"RET:"<<ret<<endl;
+        return ret;
+    }
+    
+    bool hasNext() {
+        if(!s.empty()) return true;
+        return false;
+    }
+};
