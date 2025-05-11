686a8d9d6593eab31c412f6eac48a6ed156192e9
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-05-11 18:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (35.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-     stack<char> st;
-    unordered_map<char, char> mp = {{')','('}, {']','['}, {'}','{'}};
-
+   stack<char> st;
     for (char ch : s) {
-        if (mp.count(ch)) {
-            if (st.empty() || st.top() != mp[ch]) return false;
-            st.pop();
-        } else {
+        if (ch == '(' || ch == '{' || ch == '[') {
             st.push(ch);
+        } else {
+            if (st.empty()) return false;
+            char top = st.top();
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '['))
+                return false;
+            st.pop();
         }
     }
-    return st.empty();
-    }
+    return st.empty();}
 };
