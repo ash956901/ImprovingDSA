dbaae12d14b10239d6d071c391060709eaa6b6cb
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-05-11 17:42:17 +0530

    Time: 430 ms (36.89%) | Memory: 93.4 MB (27.40%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -2,55 +2,40 @@ class Solution {
 public:
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
-        if (num.empty()) return ans;
-        dfs(num, target, 0, 0, 0, "", ans);
+        if(num.empty()) return ans;
+        dfs(num,target,0,0,0,"",ans);
         return ans;
     }
-
 private:
-    
-    void dfs(const string& num,
-             long long target,
-             int pos,
-             long long eval,
-             long long multed,
-             string path,
-             vector<string>& ans) {
-        int n = num.size();
-        if (pos == n) {
-            if (eval == target)
+    void dfs(string &num,long long target,int posn,long long eval,long long multed,string path,vector<string>&ans){
+        int n=num.size();
+        if(posn==n){
+            if(eval==target){
                 ans.push_back(path);
+                return;
+            }
             return;
         }
 
-        
-        for (int i = pos; i < n; ++i) {
-           
-            if (i > pos && num[pos] == '0') break;
+        for(int i=posn;i<n;i++){
+            
+            if(i>posn && num[posn]=='0') break;
+            long long curr=stoll(num.substr(posn,i-posn+1));
+            string currstr=num.substr(posn,i-posn+1);
 
-            long long cur = stoll(num.substr(pos, i - pos + 1));
-            string curStr = num.substr(pos, i - pos + 1);
-
-            if (pos == 0) {
-                dfs(num, target, i + 1, cur, cur, curStr, ans);
-            } else {
-                dfs(num, target, i + 1,
-                    eval + cur,
-                    cur,
-                    path + "+" + curStr,
-                    ans);
-                dfs(num, target, i + 1,
-                    eval - cur,
-                    -cur,
-                    path + "-" + curStr,
-                    ans);
-
-                dfs(num, target, i + 1,
-                    eval - multed + multed * cur,
-                    multed * cur,
-                    path + "*" + curStr,
-                    ans);
+            if(posn==0){
+                dfs(num,target,i+1,curr,curr,currstr,ans);
             }
+            else{
+                //Addition
+                dfs(num,target,i+1,eval+curr,curr,path + "+" + currstr,ans);
+                //Subtraction
+                dfs(num,target,i+1,eval-curr,-curr,path + "-" + currstr,ans);
+                //Multiplication
+                dfs(num,target,i+1,eval-multed+multed*curr,multed*curr,path + "*" + currstr,ans);
+
+            }
+
         }
     }
 };
