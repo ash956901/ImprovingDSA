f1465cbdeff1e7957ebfd977e26426a5842727ea
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-05-15 18:54:09 +0530

    Time: 30 ms (25.19%) | Memory: 11.1 MB (58.77%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int solve(string s,int k){
+    int numberOfSubstrings(string s) {
         unordered_map<char,int> freq;
         int i=0;
-        int dist=0;
         int res=0;
         for(int j=0;j<s.size();j++){
             freq[s[j]]++;
-            if(freq[s[j]]==1) dist++;
-            while(dist>k){
-                freq[s[i]]--;
-                if(freq[s[i]]==0) dist--;
-                i++;
+            while(freq['a']>0 && freq['b']>0 && freq['c']>0){
+                res+=(s.length()-j);
+                freq[s[i++]]--;
             }
-            res+=(j-i+1);
-            
+          
         }
-
         return res;
-    }
-    int numberOfSubstrings(string s) {
-        return solve(s,3)-solve(s,2);
     }
 };
