dd779255acd97b36f4f6673024afc1a222b2073c
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-06-04 19:19:36 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (95.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,52 +1,28 @@
 class Solution {
 public:
-    int getPeak(vector<int>& arr) {
-        int s = 0;
-        int e = arr.size() - 1;
-        int mid = s + (e - s) / 2;
-        while (s <e) {
-            if (arr[mid] >= arr[0] && arr[mid] >=arr[mid + 1]) {
-                s = e = mid;
-                break;
-            } else if (arr[mid] >=arr[0] && arr[mid] <=arr[mid + 1]) {
-                s = mid + 1;
-            } else if (arr[mid] <=arr[0] && arr[mid] <=arr[mid + 1]) {
-                e = mid - 1;
-            }
-            mid = s + (e - s) / 2;
-        }
-        return s;
-    }
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
 
-    int search(vector<int>& nums, int target,int start,int end) {
-        int s=start;
-        int e=end;
-        int mid=s+(e-s)/2;
-        while(s<=e){
-            if(nums[mid]==target){
-                return mid;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+           
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
             }
-            else if(target>nums[mid]){
-                s=mid+1;
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
-            else if(target<nums[mid]){
-                e=mid-1;
-            }
-            mid=s+(e-s)/2;
         }
 
         return -1;
-        
-    }
-
-    int search(vector<int>& nums, int target) {
-        int peak = getPeak(nums);
-        int firstPart=search(nums,target,0,peak);
-        int secondPart=search(nums,target,peak+1,nums.size()-1);
-        if(firstPart==-1 && secondPart==-1 ) return -1;
-        else if(firstPart>=0) return firstPart;
-            return secondPart;
-        
-
     }
 };
