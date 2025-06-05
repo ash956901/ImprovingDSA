7342ce956b8853d77e9b961222ae0cc08ec8542c
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-06-06 00:06:04 +0530

    Time: 192 ms (85.34%) | Memory: 10.4 MB (94.46%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,69 +1,34 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board,string &curr,int i,int j,string &word,vector<vector<bool>> &visited){
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size()){
-            if(curr==word) return true;
-            return false;
-        }
-        if(curr==word) {
-            return true;}
+    bool dfs(vector<vector<char>>& board, int i, int j, int index, string& word) {
+        if (index == word.size()) return true;
 
-        char cChar=word[curr.size()];
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
+            board[i][j] != word[index]) return false;
 
-        //explore all four options
-        bool a=false;
-        bool b=false;
-        bool c=false;
-        bool d=false;
+        char temp = board[i][j];
+        board[i][j] = '#';  
 
-        visited[i][j]=true;
+        bool found = dfs(board, i+1, j, index+1, word) ||
+                     dfs(board, i-1, j, index+1, word) ||
+                     dfs(board, i, j+1, index+1, word) ||
+                     dfs(board, i, j-1, index+1, word);
 
-        if(i-1>=0 && board[i-1][j]==cChar && !visited[i-1][j]){
-            curr.push_back(cChar);
-            a=solve(board,curr,i-1,j,word,visited);
-            curr.pop_back();
-        }
-
-         if(i+1<board.size() && board[i+1][j]==cChar  && !visited[i+1][j]){
-            curr.push_back(cChar);
-            b=solve(board,curr,i+1,j,word,visited);
-              curr.pop_back();
-        }
-
-        if(j+1<board[0].size() && board[i][j+1]==cChar  && !visited[i][j+1]){
-             curr.push_back(cChar);
-             c=solve(board,curr,i,j+1,word,visited);
-              curr.pop_back();
-        }
-
-
-        if(j-1>=0 && board[i][j-1]==cChar  && !visited[i][j-1]){
-             curr.push_back(cChar);
-             d=solve(board,curr,i,j-1,word,visited);
-              curr.pop_back();
-        }
-
-        visited[i][j]=false;
-        return (a||b||c||d);
-
-
+        board[i][j] = temp;  
+        return found;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
-        string temp="";
-       
-        //find the first char 
-        vector<vector<bool>> visited(board.size(),vector<bool> (board[0].size(),false));
-        bool ans=false;
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(board[i][j]==word[0]){
-                    temp.push_back(word[0]);
-                    ans=ans|| solve(board,temp,i,j,word,visited);
-                    if(ans) return true;
-                    temp.pop_back();
+        int rows = board.size();
+        int cols = board[0].size();
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (board[i][j] == word[0] && dfs(board, i, j, 0, word)) {
+                    return true;
                 }
             }
         }
-        return ans;
+        return false;
     }
 };
