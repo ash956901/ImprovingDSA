f5d9138ed82ec6dc933e53927f133e7d2025b978
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-06-12 00:46:48 +0530

    Time: 11 ms (95.17%) | Memory: 81.4 MB (72.76%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int area = 0;
-        stack<int> ng;
         int n = heights.size();
-        
-        for (int i = 0; i < n; i++) {
-            while (!ng.empty() && heights[ng.top()] > heights[i]) {
-                int topIndex = ng.top();
-                ng.pop();
-                int width = ng.empty() ? i : i - ng.top() - 1;
-                area = max(area, heights[topIndex] * width);
+        stack<int> st;
+        int maxArea = 0;
+
+       
+        for (int i = 0; i <= n; ++i) {
+            int curr = (i < n ? heights[i] : 0);
+
+            while (!st.empty() && heights[st.top()] > curr) {
+                int h = heights[st.top()]; 
+                st.pop();
+                int left = st.empty() ? 0 : st.top() + 1;
+                int width = i - left;
+                maxArea = max(maxArea, h * width);
             }
-            ng.push(i);
+            st.push(i);
         }
-        
-        while (!ng.empty()) {
-            int topIndex = ng.top();
-            ng.pop();
-            int width = ng.empty() ? n : n - ng.top() - 1;
-            area = max(area, heights[topIndex] * width);
-        }
-        
-        return area;
+
+        return maxArea;
     }
 };
