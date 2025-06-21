39b8a62bfb3cef44cf73e37f7bee2103b3d8cb31
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-06-21 17:15:56 +0530

    Time: 664 ms (94.26%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
-with minTable as (Select product_id,min(year) as first_year from Sales group by product_id)
-select m.product_id,m.first_year,s.quantity,s.price from minTable m join Sales s on m.product_id=s.product_id and m.first_year=s.year
-;
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+FROM Sales s
+JOIN (
+  SELECT product_id, MIN(year) AS first_year
+  FROM Sales
+  GROUP BY product_id
+) m ON s.product_id = m.product_id AND s.year = m.first_year;
