2d3b93b7e5fa0fc98c7d75f4ed7316f2af41a927
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-06-23 02:05:52 +0530

    Time: 11 ms (12.33%) | Memory: 19.6 MB (8.26%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,39 +2,36 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,list<int>> adj;
-        for(auto i:prerequisites){
-            adj[i[1]].push_back(i[0]);
-        }
-
-        vector<int> indegree(numCourses+1,0);
-        
-        for(auto i:adj){
-            for(auto j:i.second){
-                indegree[j]++;
-            }
-        }
-
+        vector<int> indegree(numCourses,0);
+        vector<int> topo;
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push({i});
-            }
-        }   
-        vector<int> ans;
-        while(!q.empty()){
-            auto front=q.front();
-            q.pop();
-      
-            ans.push_back(front);
 
-            for(auto i:adj[front]){
-                if(--indegree[i]==0){
-                    q.push(i);
-                }
-            }
+        for(auto i:prerequisites){
+            adj[i[0]].push_back(i[1]);
+            indegree[i[1]]++;
         }
-        if(ans.size()!=numCourses) return {};
-        return ans;
+
+        
+        for(int i=0;i<numCourses;i++){
+            if(!indegree[i]) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node=q.front();
+            topo.push_back(node);
+            q.pop();
+
+            for(auto i:adj[node]){
+                indegree[i]-=1;
+                if(indegree[i]==0) q.push(i);
+            }
+
+
+        }
+        reverse(topo.begin(),topo.end());
+        if(topo.size()==numCourses) return topo;
+        topo.clear();
+        return topo;
 
     }
 };
