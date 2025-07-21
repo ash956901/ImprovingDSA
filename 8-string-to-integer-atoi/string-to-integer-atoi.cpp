27538b67e7c57b742499d097a630370e27dfa650
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2025-07-22 01:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (24.11%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-   int myAtoi(string s) {
-    int i = 0, sign = 1, num = 0;
-    int n = s.length();
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.size();
+        long long result = 0; 
+        int sign = 1;
 
-    
-    while (i < n && s[i] == ' ') i++;
-
-    
-    if (i < n && (s[i] == '-' || s[i] == '+')) {
-        sign = (s[i] == '-') ? -1 : 1;
-        i++;
-    }
-
-   
-    while (i < n && isdigit(s[i])) {
-        int digit = s[i] - '0';
+       
+        while (i < n && s[i] == ' ') i++;
 
         
-        if (num > (INT_MAX - digit) / 10) {
-            return (sign == 1) ? INT_MAX : INT_MIN;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
         }
 
-        num = num * 10 + digit;
-        i++;
+        
+        while (i < n && isdigit(s[i])) {
+            result = result * 10 + (s[i] - '0');
+
+           
+            if (sign * result > INT_MAX) return INT_MAX;
+            if (sign * result < INT_MIN) return INT_MIN;
+
+            i++;
+        }
+
+        return static_cast<int>(sign * result);
     }
-
-    return sign * num;
-}
-
 };
-
-
